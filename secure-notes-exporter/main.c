3cc088136887678b5055e963c7f234ee44b4baa7
Greg Hurrell <greg@hurrell.net>
2014-05-09 07:13:11 -0700

    Add new Xcode project
    This is a project made with the Core Foundation command-line tool
    template.
    
    It's been so long since I seriously used Xcode and the conventional
    project structure has changed quite a bit, so I added a .gitignore file
    generated at http://www.gitignore.io/api/xcode
    
    Signed-off-by: Greg Hurrell <greg@hurrell.net>

---
+++ secure-notes-exporter/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  secure-notes-exporter
+//
+//  Created by Greg Hurrell on 5/9/14.
+//  Copyright (c) 2014 Greg Hurrell. All rights reserved.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    CFShow(CFSTR("Hello, World!\n"));
+    return 0;
+}
+
