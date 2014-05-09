28b5c302b8d65d935fb833d5a8a583866f2e5c60
Greg Hurrell <greg@hurrell.net>
2014-05-09 07:56:53 -0700

    Print something about "Secure Notes" while iterating
    I couldn't find a constant for 'note' and there are probably more
    idiomatic ways to do this, but this is what I have.
    
    Signed-off-by: Greg Hurrell <greg@hurrell.net>

--- secure-notes-exporter/main.c
+++ secure-notes-exporter/main.c
@@ -9,6 +9,18 @@
 #include <CoreFoundation/CoreFoundation.h>
 #include <Security/Security.h>
 
+void printItem(const void *value, void *context) {
+    CFDictionaryRef dictionary = value;
+    CFNumberRef itemType = CFDictionaryGetValue(dictionary, kSecAttrType);
+    CFNumberRef noteType = (CFNumberRef)context;
+    if (!itemType || !CFEqual(itemType, noteType)) {
+        // not a note
+        return;
+    }
+
+    CFShow(noteType);
+}
+
 int main(int argc, const char * argv[])
 {
     // create query
@@ -28,7 +40,13 @@ int main(int argc, const char * argv[])
     OSStatus status = SecItemCopyMatching(query, (CFTypeRef *)&items);
     CFRelease(query);
     assert(status == 0);
-    CFShow(items);
+
+    // iterate over "Secure Note" items
+    CFRange range = CFRangeMake(0, CFArrayGetCount(items));
+    SInt32 note = 'note';
+    CFNumberRef noteRef = CFNumberCreate(kCFAllocatorDefault, kCFNumberSInt32Type, &note);
+    CFArrayApplyFunction(items, range, printItem, (void *)noteRef);
+    CFRelease(noteRef);
     CFRelease(items);
     return 0;
 }
